4cd46a85eb5915c4edcf897bd35debc90766e030
BlackRabit1522 <122232787+BlackRabit1522@users.noreply.github.com>
2024-11-14 21:47:26 -0700

    Add files via upload

--- API.py
+++ API.py
@@ -12,9 +12,7 @@ jwt = JWTManager(app)
 
 @app.route('/')
 def index():
-    return jsonify({"message": "API en funcionamiento"})
+    return jsonify({"message": "MANUEL LANDEROS MADERA EDAD: ?"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
