9f9be93a46af5369d6039e3b3d9faa79f1e90844
BlackRabit1522 <122232787+BlackRabit1522@users.noreply.github.com>
2024-11-15 13:43:01 -0700

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    POSTGRES_HOST = 'localhost'  # Dirección del servidor de PostgreSQL
-    POSTGRES_DATABASE = 'dbmaestros'  # Nombre de la base de datos
-    POSTGRES_USER = 'postgres'  # Usuario de PostgreSQL (puede ser diferente)
-    POSTGRES_PASSWORD = '123'  # Contraseña de PostgreSQL (ajústala a tu configuración
+    POSTGRES_HOST = 'uadeo_api_db.oregon-postgres.render.com'  # Dirección del servidor de PostgreSQL
+    POSTGRES_DATABASE = 'uadeo_api_db'  # Nombre de la base de datos
+    POSTGRES_USER = 'apimaster'  # Usuario de PostgreSQL (puede ser diferente)
+    POSTGRES_PASSWORD = 'oyesXOc1IxXiNVEjz2oxecclfWS9OeXx'  # Contraseña de PostgreSQL (ajústala a tu configuración
