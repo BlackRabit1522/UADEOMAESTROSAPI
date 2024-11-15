64d0bdc4d58ebde4fc1050939afa37389dc66c2f
BlackRabit1522 <122232787+BlackRabit1522@users.noreply.github.com>
2024-11-14 20:26:32 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    POSTGRES_HOST = 'localhost'  # Dirección del servidor de PostgreSQL
+    POSTGRES_DATABASE = 'dbmaestros'  # Nombre de la base de datos
+    POSTGRES_USER = 'postgres'  # Usuario de PostgreSQL (puede ser diferente)
+    POSTGRES_PASSWORD = '123'  # Contraseña de PostgreSQL (ajústala a tu configuración
