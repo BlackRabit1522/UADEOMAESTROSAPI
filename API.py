0e2bd2cd50c28ba44e61deac3439c7b59d38d3bf
BlackRabit1522 <122232787+BlackRabit1522@users.noreply.github.com>
2024-11-14 21:55:48 -0700

    Add files via upload

--- API.py
+++ API.py
@@ -12,7 +12,7 @@ jwt = JWTManager(app)
 
 @app.route('/')
 def index():
-    return jsonify({"message": "EL PROXIMO AÑO SE LE PATROCINA LAS CARIÑOSAS AL MOMIN"})
+    return jsonify({"message": "EL PROXIMO CUMPLE DEL MOMIN SE LE PATROCINA LAS CARIÑOSAS"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
